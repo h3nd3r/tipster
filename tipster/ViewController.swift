b23b4ebac7309d55688d119a0249f6bd904d5602
Sara Hender <pegasus@saras-macbook.attlocal.net>
2016-09-06 10:09:56 -0700

    Disable keyboard animation, add light/dark theme.

--- tipster/ViewController.swift
+++ tipster/ViewController.swift
@@ -10,17 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var tipLabel: UILabel!
-    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
     
     override func viewDidLoad() {
         print("%s", #function)
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
         billField.becomeFirstResponder()
+        UIWindow.setAnimationsEnabled(false)
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let theme = defaults.boolForKey("theme")
+        updateColorScheme(theme)
+        print("theme: \(theme)")
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -31,16 +38,22 @@ class ViewController: UIViewController {
         tipControl.selectedSegmentIndex = defaults.integerForKey("tipControl")
         
         calculateTip(self)
+        
+        let theme = defaults.boolForKey("theme")
+        updateColorScheme(theme)
+        print("theme: \(theme)")
     }
     
     override func viewDidAppear(animated: Bool) {
         print("%s", #function)
         super.viewDidAppear(animated)
+        UIWindow.setAnimationsEnabled(false)
     }
     
     override func viewWillDisappear(animated: Bool) {
         print("%s", #function)
         super.viewWillDisappear(animated)
+        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
     
     override func viewDidDisappear(animated: Bool) {
@@ -60,8 +73,45 @@ class ViewController: UIViewController {
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalAmountLabel.text = String(format: "$%.2f", total)
     }
+
+    func keyboardShown(notification: NSNotification) {
+        let info  = notification.userInfo!
+        let value: AnyObject = info[UIKeyboardFrameEndUserInfoKey]!
+        
+        let rawFrame = value.CGRectValue()
+        let keyboardFrame = view.convertRect(rawFrame, fromView: nil)
+        
+        print("keyboardFrame: \(keyboardFrame)")
+    }
+    
+    func updateColorScheme(color: Bool) {
+        if color {
+            self.view.backgroundColor = UIColor.blackColor()
+            tipControl.tintColor = UIColor.grayColor()
+            self.view.tintColor = UIColor.grayColor()
+            tipLabel.textColor = UIColor.whiteColor()
+            totalLabel.textColor = UIColor.whiteColor()
+            //billField.textColor = UIColor.whiteColor()
+            billLabel.textColor = UIColor.whiteColor()
+            tipAmountLabel.textColor = UIColor.whiteColor()
+            totalAmountLabel.textColor = UIColor.whiteColor()
+        }
+        else {
+            self.view.backgroundColor = UIColor.whiteColor()
+            self.view.tintColor = UIColor.grayColor()
+            tipControl.tintColor = UIColor.grayColor()
+            tipLabel.textColor = UIColor.blackColor()
+            totalLabel.textColor = UIColor.blackColor()
+            //billField.textColor = UIColor.blackColor()
+            billLabel.textColor = UIColor.blackColor()
+            tipAmountLabel.textColor = UIColor.blackColor()
+            totalAmountLabel.textColor = UIColor.blackColor()
+        }
+        
+    }
+    
 }
 
